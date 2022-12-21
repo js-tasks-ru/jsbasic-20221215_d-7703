8ac964db31c60b9ea0c50fc26e37e52400b6561c
Dmitriy Opekushin <d.7703@yandex.ru>
2022-12-21 18:29:39 +0300

    change function

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,6 +1,6 @@
 function ucFirst(str) {
   if (str.length === 1) return str.toUpperCase();
-  if (str === "") return str;
+  if (!str) return str;
 
   let strStart = str[0].toUpperCase();
   let strEnd = str.slice(1);
