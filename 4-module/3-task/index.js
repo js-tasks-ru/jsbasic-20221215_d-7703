6b8b2e6e437b7f264d9961ed7f2b29b09a285f6c
Dmitriy Opekushin <d.7703@yandex.ru>
2023-01-08 20:30:18 +0300

    module 4, task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let i = 1; i < table.rows.length; i++) {
+    for (let j = 0; j < table.rows[i].cells.length; j++) {
+      if (j === 1) {
+        if (table.rows[i].cells[j].innerHTML < 18) {
+          table.rows[i].style.textDecoration = 'line-through';
+        }
+      }
+      if (j === 2) {
+        if (table.rows[i].cells[j].innerHTML === 'm') {
+          table.rows[i].classList.add('male');
+        }
+        else {
+          table.rows[i].classList.add('female');
+        }
+      }
+      if (j === 3) {
+        if (!table.rows[i].cells[j].dataset.available) {
+          table.rows[i].hidden = 'true';
+        }
+        else if (table.rows[i].cells[j].dataset.available === 'true') {
+          table.rows[i].classList.add('available');
+        }
+        else {
+          table.rows[i].classList.add('unavailable');
+        }
+      }
+    }
+  }
+  
 }
