0c80ac7d0588105e5aa41b6c993c6edb428b6af8
Dmitriy Opekushin <d.7703@yandex.ru>
2023-01-08 19:07:34 +0300

    module 4, task 1 complete

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  let ul = document.createElement('UL');
+
+  for (let friend of friends) {
+    ul.insertAdjacentHTML('beforeend', `
+      <li>${friend.firstName} ${friend.lastName}</li>
+    `);
+  }
+  
+  return ul;
 }
