7bd563449a2b89b15ba803508acad3da730e3592
Dmitriy Opekushin <d.7703@yandex.ru>
2023-01-15 18:11:49 +0300

    module 5, task 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,64 @@
 function initCarousel() {
-  // ваш код...
+  
+  const carousel = document.querySelector('.carousel'); // основная обретка карусели
+  const btnArrowRight = document.querySelector('.carousel__arrow_right'); // стрелка вправо
+  const btnArrowLeft = document.querySelector('.carousel__arrow_left'); // стрелка влево
+  const carouselInner = document.querySelector('.carousel__inner'); // обертка слайдов
+  let innerWidth = carouselInner.offsetWidth; // вычисление ширины обертки
+  let carouselSlides = document.querySelectorAll('.carousel__slide'); // чтобы узнать кол-во слайдов
+  let slideClick = 1; // переменная, чтобы знать, сколько слайдов прошло (на каком сейчас слайде)
+  
+
+  // функция, которая скрывает стрелки
+  function arrowHide(slide) {
+    if (carouselSlides.length === 1) {
+      btnArrowLeft.style.display = 'none';
+      btnArrowRight.style.display = 'none';
+    }
+    else if (slide === 1) {
+      btnArrowLeft.style.display = 'none';
+    }
+    else if (slide === carouselSlides.length) {
+      btnArrowRight.style.display = 'none';
+    }
+    else if (slide != 1) {
+      btnArrowLeft.style.display = '';
+      btnArrowRight.style.display = '';
+    }
+  }
+
+  // запуск функции, чтобы левая стрелка скрылась при открытии страницы
+  arrowHide(slideClick);
+
+  // объект слайдер с методами для передвижения слайдов
+  let slider = {
+    slideR() {
+      if (slideClick === carouselSlides.length) {
+        return;
+      };
+      carouselInner.style.transform = `translateX(-${innerWidth * slideClick}px)`;
+      slideClick++;
+      arrowHide(slideClick);
+    },
+
+    slideL() {
+      if (slideClick === 1) {
+        return;
+      };
+      slideClick--;
+      carouselInner.style.transform = `translateX(${ innerWidth - innerWidth * slideClick}px)`;
+      arrowHide(slideClick);
+    }
+  }
+
+  // вызов переключений
+  carousel.addEventListener('click', (event) => {
+    let target = event.target.closest('div');
+    let arrowR = target.classList.contains('carousel__arrow_right');
+    let arrowL = target.classList.contains('carousel__arrow_left');
+    
+    if (arrowR) slider.slideR();
+    if (arrowL) slider.slideL();
+  });
+  
 }
