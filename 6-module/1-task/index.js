8828969573b45bb36b881b20937630f0b4d25718
Dmitriy Opekushin <d.7703@yandex.ru>
2023-01-18 17:11:49 +0300

    module 5, task 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,62 @@
  *      }
  *
  */
+import createElement from '../../assets/lib/create-element.js';
+
 export default class UserTable {
   constructor(rows) {
+    let arr = rows.map(row => 
+      `
+        <tr>
+          <td>
+            ${row.name}
+          </td>
+          <td>
+            ${row.age}
+          </td>
+          <td>
+            ${row.salary}
+          </td>
+          <td>
+            ${row.city}
+          </td>
+          <td>
+            <button>X</button>
+          </td>
+        </tr>    
+      `
+    );
+
+    this.elem = createElement(`
+      <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          ${arr}
+        </tbody>
+      </table>
+    `
+    )
+   
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.elem.addEventListener('click', event => {
+      let target = event.target.closest('button');
+      let td = target.parentElement;;
+      let tr = td.parentElement;
+      
+      if (target) {
+        tr.remove();
+      }
+    });
   }
 }
